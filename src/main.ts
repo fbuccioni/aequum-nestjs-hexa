0cbdfce7103fcb091c970036e8fd706e6e8ee1d7
Felipe Buccioni <falcacibar@gmail.com>
2024-12-05 14:54:25 -0300

    improve(system)!: Use `swc` for faster TS compilation BREAKING CHANGE: Please update packages

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
     const app = await NestFactory.create<NestFastifyApplication>(
         AppModule,
         new FastifyAdapter()
-    );
+    );  
 
     app.use(morgan('dev'));
     app.useGlobalPipes(
