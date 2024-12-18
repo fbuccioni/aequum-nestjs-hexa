bc0331db3c5b7aa4044be42bda4c913149458dc5
Felipe Buccioni <falcacibar@gmail.com>
2024-12-18 02:06:38 -0300

    improve: container (Docker) and environment staff

--- src/shared/common/utils/env.utils.ts
+++ src/shared/common/utils/env.utils.ts
@@ -6,25 +6,6 @@ export function load() {
     expand(dotenv.config());
 }
 
-export function hostAndPort(listen: string): [ string, number ] {
-    let host: string = undefined;
-    let port: number = undefined;
-
-    if (!listen) return [ host, port ];
-
-    if (/^\d+$/.test(listen)) port = +listen;
-    else if (/:/.test(listen))
-        [ host, port ] = listen
-            .split(':')
-            .map((e, i) => i === 1 ? +e : e) as [ string, number ];
-    else host = listen;
-
-    if (!port) port = 8085;
-    if (!host) host = 'localhost';
-
-    return [ host, port ];
-}
-
 export function asBoolean(s: string) {
     return ['y', 'yes', '1', 'true', 't'].includes(s.toLowerCase())
 }
