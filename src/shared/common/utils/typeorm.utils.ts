8dfa8671d4ef74307fbcc4aa55ccd0a937383507
Felipe Buccioni <falcacibar@gmail.com>
2024-12-20 12:20:03 -0300

    improve: Some shortanhds to find duplicates/uniques in TypeORM util

--- src/shared/common/utils/typeorm.utils.ts
+++ src/shared/common/utils/typeorm.utils.ts
@@ -1,4 +1,6 @@
-import { DataSourceOptions } from 'typeorm';
+import { DataSourceOptions, TypeORMError } from 'typeorm';
+
+import { DuplicateEntryException } from '../exceptions/data/duplicate-entry.exception';
 
 
 export function URIToDataSourceOptions(uri: string):  DataSourceOptions {
@@ -34,4 +36,26 @@ export function URIToDataSourceOptions(uri: string):  DataSourceOptions {
         port: (+parsedURI.port) || undefined,
         ... additionalConfig
     };
+}
+
+export function isDuplicateError(err: any) {
+    return (
+        err instanceof TypeORMError
+        && err.message.match(/(duplicate|unique)/)
+    )
+}
+
+export function duplicateEntryExceptionOrError<T extends Error>(
+    err: T, message?: any,
+    data?: any, duplicatedProperties?: string[]
+): T | DuplicateEntryException {
+    if (isDuplicateError(err))
+        return new DuplicateEntryException(
+            message,
+            data,
+            duplicatedProperties,
+            err
+        );
+
+    return err;
 }
