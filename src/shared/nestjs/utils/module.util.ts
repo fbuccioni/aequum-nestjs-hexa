b1f9aed406e9acd462ad1c6f358b0f636358a6ff
Felipe Buccioni <falcacibar@gmail.com>
2024-12-20 10:53:17 -0300

    fix: `moduleUtil` fixes

--- src/shared/nestjs/utils/module.util.ts
+++ src/shared/nestjs/utils/module.util.ts
@@ -1,18 +1,13 @@
-import { DynamicModule, ForwardReference } from '@nestjs/common';
-
-
-type ModuleLike = DynamicModule | Promise<DynamicModule> | ForwardReference<any>
-
-export function toArray<T extends {}>(m: T, recursive = true): ModuleLike[] {
+export function toArray(m: any, recursive = true): unknown[] {
     const moduleArray = Object.values(m)
         .filter((m) => typeof m === 'function' || (recursive && Array.isArray(m)));
 
     if (recursive)
         moduleArray.map((e) => typeof e === 'object' ? toArray(e as {}) : e);
 
-    return moduleArray as ModuleLike[];
+    return moduleArray as unknown[];
 }
 
-export function toFlattenArray<T extends {}>(m: T): ModuleLike[] {
-    return Array.from(toArray(m).flat());
+export function toFlattenArray(m: any) {
+    return Array.from(toArray(m).flat()) as unknown[];
 }
