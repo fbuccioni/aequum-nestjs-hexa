52072baff3bdf65c11b323ef1f858c2993bb6860
Felipe Buccioni <falcacibar@gmail.com>
2024-12-29 17:11:23 -0300

    fix(application/api): Change to NestJS plural convention for modules

--- src/application/api/api-modules.export.ts
+++ src/application/api/api-modules.export.ts
@@ -1,4 +1,4 @@
 // The list of the modules that will be included in the app,
 // also are recursive.
 
-export * from './example/example.module';
+export * from './examples/examples.module';
--- src/application/api/examples/examples.module.ts
+++ src/application/api/examples/examples.module.ts
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 
 
 @Module({})
-export class ExampleModule {
+export class ExamplesModule {
 
 }
