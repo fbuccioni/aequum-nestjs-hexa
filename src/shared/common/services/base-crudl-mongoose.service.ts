0327b376125fd71d7040d0999ec3ef7da41f64ac
Felipe Buccioni <falcacibar@gmail.com>
2024-12-30 15:54:04 -0300

    improve(shared/common): `BaseCRUDLMongooseService` now catches duplicates

--- src/shared/common/services/base-crudl-mongoose.service.ts
+++ src/shared/common/services/base-crudl-mongoose.service.ts
@@ -1,4 +1,6 @@
 import { MongooseRepository } from "../repositories/mongoose.repository";
+import { duplicateEntryExceptionOrError } from '../utils/mongoose.util';
+
 import { BaseCRUDLService } from "./base-crudl.service";
 
 
@@ -8,10 +10,33 @@ export abstract class BaseCRUDLMongooseService<
     SchemaModelCreateDto,
     SchemaModelUpdateDto
 > extends BaseCRUDLService implements BaseCRUDLService {
+    static uniqueFields: string[];
+    static duplicatedEntryMessage?: string;
+
+    static duplicateEntryExceptionMessage() {
+        const self = this;
+
+        if (self.duplicatedEntryMessage)
+            return self.duplicatedEntryMessage;
+
+        if (this.uniqueFields && this.uniqueFields?.length)
+            return `\`${self.uniqueFields.join('` or ')}\` already exists`;
+
+        return 'Duplicated entry';
+    }
+
     protected readonly repository: MongooseRepository<SchemaModel>
 
     async create(data: SchemaModelCreateDto): Promise<SchemaModelDto> {
-        return this.repository.put(data as unknown as SchemaModel) as SchemaModelDto;
+        const self = this.constructor as typeof BaseCRUDLMongooseService;
+
+        try {
+            return this.repository.put(data as unknown as SchemaModel) as SchemaModelDto;
+        } catch (err) {
+            throw duplicateEntryExceptionOrError(
+                err, self.duplicateEntryExceptionMessage(), data, self.uniqueFields || []
+            )
+        }
     }
 
     async retrieve(id: SchemaModel['id']): Promise<SchemaModelDto> {
@@ -19,8 +44,16 @@ export abstract class BaseCRUDLMongooseService<
     }
 
     async update(id: SchemaModel['id'], data: SchemaModelUpdateDto): Promise<SchemaModelDto> {
-        await this.repository.update({ _id: id }, data);
-        return this.retrieve(id) as unknown as SchemaModelDto;
+        const self = this.constructor as typeof BaseCRUDLMongooseService;
+
+        try {
+            await this.repository.update({ _id: id }, data);
+            return this.retrieve(id) as unknown as SchemaModelDto;
+        } catch (err) {
+            throw duplicateEntryExceptionOrError(
+                err, self.duplicateEntryExceptionMessage(), data, self.uniqueFields || []
+            );
+        }
     }
 
     async delete(id: SchemaModel['id']): Promise<void> {
