71787f5d7caacdf88562849e7e66f8074d775c91
Felipe Buccioni <falcacibar@gmail.com>
2024-12-30 14:09:40 -0300

    fix(common/nestjs): Utils must be just in one place

--- src/application/api/app.module.ts
+++ src/application/api/app.module.ts
@@ -3,7 +3,7 @@ import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
 
-import * as moduleUtil from '../../shared/nestjs/utils/module.util';
+import * as moduleUtil from '../../shared/nestjs/common/utils/module.util';
 import { LoggerInterceptor } from '../../shared/nestjs/logger/interceptors';
 import { LoggerModule } from '../../shared/nestjs/logger/logger.module';
 import { HttpResponseModule } from '../../shared/nestjs/http-response/http-response.module';
