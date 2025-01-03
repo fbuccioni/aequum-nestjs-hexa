98551f4af67c68dae646848137c0401ee610d43c
Felipe Buccioni <falcacibar@gmail.com>
2025-01-03 14:36:12 -0300

    fix(infrastructure/database): Support for `synchronize` to TypeORM via `DATABASE_MAIN_SYNCHRONIZE` env

--- src/infrastructure/database/typeorm.config.ts
+++ src/infrastructure/database/typeorm.config.ts
@@ -13,13 +13,18 @@ env.load()
 const migrationsDir = `${__dirname}/migrations`;
 const typeORMConfiguration = {
     ...URIToDataSourceOptions(process.env.DATABASE_MAIN_URI),
-    synchronize: false,
+    synchronize: env.asBoolean(process.env.DATABASE_MAIN_SYNCHRONIZE),
     entities: moduleUtils.toFlattenArray(Entities) as EntitySchema[],
-    migrations : [ `${migrationsDir}/**/*.ts` ],
-    migrationsTableName: 'typeorm_migrations',
-    migrationsRun: env.asBoolean(process.env.DATABASE_MAIN_MIGRATIONS_AUTO),
     logging: env.asBoolean(process.env.DATABASE_MAIN_LOGGING),
     cli: { migrationsDir }
 }
+
+// When synchronize is not explicitly set, fall into migration
+if (!typeORMConfiguration.synchronize)
+    Object.assign(typeORMConfiguration, {
+        migrations : [ `${migrationsDir}/**/*.ts` ],
+        migrationsTableName: 'typeorm_migrations',
+        migrationsRun: env.asBoolean(process.env.DATABASE_MAIN_MIGRATIONS_AUTO),
+    });
 
 export default typeORMConfiguration;
