d17406d945a48c1f5c6dd33337d5c048ebf0e49e
Felipe Buccioni <falcacibar@gmail.com>
2025-01-03 15:01:22 -0300

    improve(application/examples): Now example application uses our CRUDL services and controllers (Mongoose)

--- src/application/services/examples.service.ts
+++ src/application/services/examples.service.ts
@@ -1,43 +1,16 @@
-import mongoose from 'mongoose';
 import { Injectable } from '@nestjs/common';
-
-import { ExampleRepository } from '../../infrastructure/database/repositories/example.repository';
 
 import { ExampleCreateDto, ExampleDto, ExampleUpdateDto } from '../dtos';
 
+import { ExampleRepository } from '../../infrastructure/database/repositories/example.repository';
+import { Example } from '../../infrastructure/database/schemas/example.schema';
+
+import { BaseCRUDLMongooseService } from '../../shared/common/services/base-crudl-mongoose.service';
+
 
 @Injectable()
-export class ExamplesService {
-    constructor(private readonly exampleRepository: ExampleRepository) {}
-
-    async get(id: string): Promise<ExampleDto> {
-        return this.exampleRepository.getOneById(
-            new mongoose.Types.ObjectId(id)
-        ) as unknown as Promise<ExampleDto>;
-    }
-
-    async list(filter?: {}): Promise<ExampleDto[]> {
-        return this.exampleRepository.find(filter) as unknown as Promise<
-            ExampleDto[]
-        >;
-    }
-
-    async new(exampleCreateDto: ExampleCreateDto): Promise<ExampleDto> {
-        return this.exampleRepository.put(
-            exampleCreateDto
-        ) as unknown as Promise<ExampleDto>;
-    }
-
-    async update(id: string, exampleUpdateDto: ExampleUpdateDto) {
-        await this.exampleRepository.update(
-            { _id: new mongoose.Types.ObjectId(id) },
-            { $set: exampleUpdateDto }
-        );
-    }
-
-    async delete(id) {
-        await this.exampleRepository.delete({
-            _id: new mongoose.Types.ObjectId(id),
-        });
+export class ExamplesService extends BaseCRUDLMongooseService<Example, ExampleDto, ExampleCreateDto, ExampleUpdateDto> {
+    constructor(protected readonly repository: ExampleRepository) {
+        super();
     }
 }
