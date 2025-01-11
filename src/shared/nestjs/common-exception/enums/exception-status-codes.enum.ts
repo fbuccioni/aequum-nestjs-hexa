ded8156589c5b1e45b8e84c5f223cb00779f2cba
Felipe Buccioni <falcacibar@gmail.com>
2025-01-11 19:00:00 -0300

    refactor(shared/nestjs/common-exception): Added authentication exceptions to status codes list, also drops the dependency on exceptions, just use the name

--- src/shared/nestjs/common-exception/enums/exception-status-codes.enum.ts
+++ src/shared/nestjs/common-exception/enums/exception-status-codes.enum.ts
@@ -1,14 +1,12 @@
-import { ValidationException } from '../../../common/exceptions/validation/validation.exception';
-import { DuplicateEntryException } from '../../../common/exceptions/data/duplicate-entry.exception';
-import { NotFoundException } from '../../../common/exceptions/data/not-found.exception';
-import { UnauthorizedException } from "../../../common/exceptions/auth/unauthorized.exception";
-
 /**
  * Maps the exception class to its status code
  */
 export const ExceptionStatusCodes = {
-    [ValidationException.name]: 400,
-    [DuplicateEntryException.name]: 409,
-    [NotFoundException.name]: 404,
-    [UnauthorizedException.name]: 401,
+    ValidationException: 400,
+    DuplicateEntryException: 409,
+    NotFoundException: 404,
+    UnauthorizedException: 401,
+
+    TokenExpiredException: 401,
+    AuthenticationFailException: 401,
 }
