e5931232f6492bc1106646926d765cb484dc9308
Felipe Buccioni <falcacibar@gmail.com>
2025-01-16 15:42:40 -0300

    improve(shared/common/utils): `envUtils.asBoolean()` now have a default value and some improvements

--- src/shared/common/utils/env.utils.ts
+++ src/shared/common/utils/env.utils.ts
@@ -6,6 +6,12 @@ export function load() {
     expand(dotenv.config());
 }
 
-export function asBoolean(s: string) {
-    return ['y', 'yes', '1', 'true', 't'].includes(s.toLowerCase())
+export function asBoolean(s: any, defaultValue: boolean = false): boolean {
+    if (typeof(s) === 'string')
+        return ['y', 'yes', '1', 'true', 't'].includes(s.toLowerCase())
+
+    if (['boolean', 'number'].includes(typeof s))
+        return !!s
+
+    return defaultValue
 }
