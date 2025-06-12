b6bc7ba0fe0be689b32e87aeabebc2a546f1f8a6
Felipe Buccioni <falcacibar@gmail.com>
2025-06-12 13:44:48 -0400

    refactor: New custom repository way

--- src/application/api/examples/examples.module.ts
+++ src/application/api/examples/examples.module.ts
@@ -4,13 +4,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Example } from '../../../infrastructure/database/entities/example.entity';
+import { ExampleRepository } from '../../../infrastructure/database/repositories/example.repository';
 import { ExamplesService } from '../../services/examples.service';
 import { ExamplesController } from './controllers/examples.controller';
 
 
 @Module({
     imports: [ TypeOrmModule.forFeature([ Example ]) ],
-    providers: [ ExamplesService ],
+    providers: [ ExamplesService, ExampleRepository ],
     controllers: [ ExamplesController ],
     exports: [ ExamplesService ],
 })
--- src/infrastructure/database/repositories/example.repository.ts
+++ src/infrastructure/database/repositories/example.repository.ts
@@ -3,14 +3,14 @@
 import { DataSource } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 import { TypeORMRepository } from '@aequum/typeorm/repositories';
+import { InjectDataSource } from '@nestjs/typeorm';
 
 import { Example } from '../entities/example.entity';
 
 
-
 @Injectable()
 export class ExampleRepository extends TypeORMRepository<Example> {
-    constructor(private dataSource: DataSource) {
+    constructor(@InjectDataSource() dataSource: DataSource) {
         super(Example, dataSource);
     }
 }
